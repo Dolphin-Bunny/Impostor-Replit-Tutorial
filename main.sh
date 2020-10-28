26d57a67150059a1f5ff1fb9fa25143a806ae483
Dolphin-Bunny <49925062+Dolphin-Bunny@users.noreply.github.com>
2020-10-28 10:59:02 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 echo Your repls ip address is:
-getent hosts impostor-building-from-source.dolphinbunny.repl.co | awk '{ print $1 }'
+getent hosts <Your Repl Name>.<Your Repl.it Username>.repl.co | awk '{ print $1 }'
 
 cp config.json ~/impostor_src/Impostor-dev/src/Impostor.Server/bin/Debug/net5.0/linux-x64/config.json
 
